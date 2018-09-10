b9d34103dc6f725c9605855448add6b062320bb6
Myke Phillips <info@thefraternity.co.uk>
2018-09-10 17:37:40 +0100

    Set up data for collection view.

---
+++ Coder-Swag/Model/Product.swift
@@ -0,0 +1,29 @@
+//
+//  Product.swift
+//  Coder-Swag
+//
+//  Created by Myke Phillips on 10/09/2018.
+//  Copyright © 2018 Myke Phillips. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+
+    init(title: String, price: String, imageName: String) {
+
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+
+
+    }
+
+
+
+
+}
---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Myke Phillips on 10/09/2018.
+//  Copyright © 2018 Myke Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var ProductTitle: UILabel!
+    @IBOutlet weak var ProductPrice: UILabel!
+    
+    
+}
